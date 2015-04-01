c46107ba9c41c8b4cfb8bddb4416bf499df53775
Petros Markou <markoupetr@skroutz.gr>
2015-04-01 16:32:35 +0300

    Change the settings passed in Greeklish#converter Use more strict settings in the greeklish_spec file, so we can check the results accurately

--- spec/greeklish_spec.rb
+++ spec/greeklish_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 
 describe 'Greeklish' do
   it "correctly converts to greeklish" do
-    converter = Greeklish.converter(max_expansions: 10,
-                                    generate_greek_variants: true)
+    converter = Greeklish.converter(max_expansions: 2,
+                                    generate_greek_variants: false)
 
     words = converter.convert("ομπρελα")
 
+    expect(words.length).to eq(2)
     expect(words).to include("omprela", "obrela")
   end
 end
